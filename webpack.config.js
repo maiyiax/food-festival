73b653ee5c804356de2291c936fc95efa2c30ff2
Mai Yia Xiong <maiyiax@gmail.com>
2021-01-22 04:31:38 -0600

    optimized photos

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,44 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
     // first, declare entry point (root of the bundle and beginning of dependency graph)
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         // second, list output where the bundled code will be placed
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist',
+    },
+    module: {
+        rules: [
+            {
+                // preprocess any file with extension .jpg
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        // where actual loader is implemented
+                        loader: 'file-loader',
+                        options: {
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets/")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     // third, provide mode we want webpack to run (default = production mode)
     plugins: [
@@ -15,6 +46,9 @@ module.exports = {
             $: "jquery",
             jQuery: 'jquery'
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder called report.html
+        })
     ],
     mode: 'development'
 };
